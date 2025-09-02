52ce1cc1155084d58ee062bb692e7691a2ccd67b
louis smith <smith9lu@mail.uc.edu>
2025-09-02 11:01:12 -0400

    task complete

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String trash = "";
+
+        System.out.print("Enter your birth month [1-12]: ");
+        if(in.hasNextInt()){
+            int birthMonth = in.nextInt();
+            if(birthMonth >= 1 && birthMonth <= 12){
+                System.out.print("Enter your birth month is: " + birthMonth);
+            }else {
+                System.out.println("You said your birth month was: " + birthMonth);
+                System.out.println("Run the program again and enter a valid birth month in the range of 1 - 12!");
+            }
+
+        } else {
+            trash = in.nextLine();
+            System.out.println("You said your birth month was: " + trash);
+            System.out.println("Run the program again and enter a valid birth month!");
+        }
+    }
+}
